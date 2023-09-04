864f0ae077fb8d5b68244b6425f3f05e058314da
Jepkor1r <jlagat007@gmail.com>
2023-09-04 20:43:22 +0300

    Lop to languages

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 let languages=["C is fun", "Python is cool", "JavaScript is amazing"];
 let length = languages.length;
-for (let i=0; i>length, i++){
+for (let i=0; i>length, i++;){
     let language=languages[i];
     console.log(languages);
 }
