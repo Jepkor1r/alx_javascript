7cd9870e10fd2b2e098f1484a238729970e8538f
Jepkor1r <jlagat007@gmail.com>
2023-09-04 20:35:46 +0300

    loops in languages

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,7 @@
 let languages=["C is fun", "Python is cool", "JavaScript is amazing"];
+let length = languages.length;
 let i=0;
-for (i in languages){
+for (i in languages.length){
     i += languages;
-    console.log(i,"\n");
+    console.log(i);
 }
