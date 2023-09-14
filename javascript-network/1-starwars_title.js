5577b28b5cc03e192575a50aaea29cac43b38a83
Jepkor1r <jlagat007@gmail.com>
2023-09-14 13:20:00 +0300

    Star wars movie title

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -6,8 +6,7 @@ const id = process.argv[2];
 
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-request.get(url, { encoding:'utf-8'})
-    .on('data', data => {
-        const response = JSON.parse(data);
-        console.log(response.title);
-    })
+request.get(url, function(error, response, body) {
+    const object = JSON.parse(body);
+    console.log(object.title);
+});
