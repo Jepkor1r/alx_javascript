5bdb630cbb282699059e9848f1ff889fae94f876
Jepkor1r <jlagat007@gmail.com>
2023-09-14 23:18:44 +0300

    Star wars Wedge Antilles

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -4,6 +4,8 @@ const request = require('request');
 
 const url = process.argv[2];
 const character_id = 18;
+const character_url = `https://swapi-api.alx-tools.com/api/people/${character_id}/`;
+
 
 request.get(url, { encoding: 'UTF-8' }, (error, response, body) => {
   if (error) {
@@ -12,7 +14,9 @@ request.get(url, { encoding: 'UTF-8' }, (error, response, body) => {
   }
 
   const films = JSON.parse(body).results;
-  const count = films.filter((film) => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${character_id}/`)).length;
+  const count = films.filter((film) =>
+  film.characters.some((character) => character === character_url)
+).length;
 
-  console.log(`${count}`);
+  console.log(count);
 });
